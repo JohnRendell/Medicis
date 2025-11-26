96339d8ac8485a2a89da7bd50584a68a82e95ee9
gelni0221 <antipolonigel@gmail.com>
2025-11-26 17:07:28 +0800

    patient login session, patient info view, and update

---
+++ src/server/models/patient_session.js
@@ -0,0 +1,23 @@
+const db = require("../config/db");
+
+async function getUserById(userId) {
+  const [rows] = await db.query(
+    "SELECT * FROM user_account WHERE user_id = ?",[userId]);
+  return rows[0];  
+}
+
+async function getPatientById(userId) {
+  const [rows] = await db.query(
+    "SELECT * FROM patient WHERE user_id = ?",[userId]);
+  return rows[0];  
+}
+
+// (async () => {
+//   const patient = await getPatientById(1);
+//   console.log(patient);
+// })();
+
+// node src/server/models/patient_session.js
+
+
+module.exports = { getUserById,getPatientById };
